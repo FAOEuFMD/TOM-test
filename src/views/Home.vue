53da6cd377d46769e25d456810c986a762864217
Sophie <sophie@sssoja.com>
2024-08-14 15:26:56 +0200

    feat: create homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,39 +1,22 @@
 <template>
   <div class="home">
-    <h1>Welcome to the Home Page</h1>
-    <p>
-      This is a simple example of a Vue 3 component with TypeScript and the
-      Composition API.
+    <h1 class="h1 text-sage-600">Welcome to TOM</h1>
+    <p class="subheader">
+      A training management system for veterinary professionals by the EuFMD.
     </p>
-    <button @click="incrementCounter">Click me! ({{ counter }})</button>
   </div>
 </template>
 
-<script setup lang="ts">
-import { ref } from "vue";
-
-// Define a reactive counter variable
-const counter = ref(0);
-
-// Define a method to increment the counter
-const incrementCounter = () => {
-  counter.value++;
-};
-</script>
+<script setup lang="ts"></script>
 
 <style scoped>
 .home {
-  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
   padding: 20px;
-}
-
-h1 {
-  color: #42b983;
-}
-
-button {
-  font-size: 1.2em;
-  padding: 10px 20px;
-  cursor: pointer;
+  box-sizing: border-box;
 }
 </style>
