2daefee57d2c6b30622edd0c6c6f61d4ec0e2007
Sophie <sophie@sssoja.com>
2024-08-28 16:10:36 +0200

    styling: fix layout of video content and subheader

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,36 +1,52 @@
 <template>
-  <div class="home">
-    <h1 class="h1 text-primary-600">Welcome to TOM</h1>
+  <div
+    class="content-container mx-auto max-w-7xl px-4 mt-12 md:mt-16 lg:mt-24 xl:mt-32 h-full"
+  >
+    <h1 class="h1">Welcome to TOM</h1>
     <p class="subheader">
-      A training management system for veterinary professionals by the EuFMD
+      TOM is a training management system that enables countries to oversee the
+      individual training and development of their veterinary professionals and
+      enhance veterinary capacity. It serves as a competency framework, offering
+      recognition to veterinarians, and operates as a web-based tool for
+      monitoring training completion within veterinary services.
     </p>
-    <iframe
-      class="iframe"
-      src="https://www.youtube.com/embed/hvK72zj-6sA?autoplay=0&rel=0&showinfo=0&modestbranding=1&controls=1"
-      frameborder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowfullscreen
-    >
-    </iframe>
+    <div class="iframe-wrapper mt-4 md:mt-8 lg:mt-6">
+      <iframe
+        class="iframe"
+        src="https://www.youtube.com/embed/hvK72zj-6sA?autoplay=0&rel=0&showinfo=0&modestbranding=1&controls=1"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts"></script>
 
 <style scoped>
-.home {
+.content-container {
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
   height: 100%;
-  padding: 20px;
   box-sizing: border-box;
+  overflow-y: auto;
+  overflow-y: scroll; /* Enable scrolling */
+  scrollbar-width: none; /* For Firefox */
+}
+
+.iframe-wrapper {
+  position: relative;
+  width: 100%;
+  padding-top: 56.25%; /* 16:9 Aspect Ratio */
+  overflow: hidden;
 }
 
 .iframe {
-  width: 560px;
-  height: 315px;
-  margin-top: 20px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
 }
 </style>
