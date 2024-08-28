475002b17eee0d46736b40b217e036b06e43a03c
Sophie <sophie@sssoja.com>
2024-08-28 13:32:35 +0200

    feat: add video content to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,16 @@
   <div class="home">
     <h1 class="h1 text-primary-600">Welcome to TOM</h1>
     <p class="subheader">
-      A training management system for veterinary professionals by the EuFMD.
+      A training management system for veterinary professionals by the EuFMD
     </p>
+    <iframe
+      class="iframe"
+      src="https://www.youtube.com/embed/hvK72zj-6sA?autoplay=0&rel=0&showinfo=0&modestbranding=1&controls=1"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    >
+    </iframe>
   </div>
 </template>
 
@@ -18,5 +26,11 @@
   height: 100%;
   padding: 20px;
   box-sizing: border-box;
+}
+
+.iframe {
+  width: 560px;
+  height: 315px;
+  margin-top: 20px;
 }
 </style>
